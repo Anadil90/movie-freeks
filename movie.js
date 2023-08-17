ab2c72b1e86cd76800d5209a093352b906762c5f
anadil90 <mashuk.anadil@yahoo.com>
2023-08-17 18:32:16 +0200

    Initialisation of the first basic working version. Put in correct key and got response from the api.

---
+++ movie.js
@@ -0,0 +1,38 @@
+
+
+const API_KEY = 24358589; //use api key for the OMDB api
+const searchButton = document.getElementById('searchButton') //search movie
+const searchTitle = document.getElementsByClassName("search-input")[0]; //movie title input
+
+searchButton.addEventListener('click', () => {
+
+  const movieTitle = searchTitle.value; //get value from input
+  
+  if(movieTitle){
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`) //fetch response from api
+    .then(response => response.json())
+    .then(data => {
+      if(data.Response === 'True') {
+        //default container to display the movie search results
+        const movieInfo = ` 
+        <div class="movie-info">
+        <h2>${data.Title}</h2>
+        <p><strong>Year:</strong> ${data.Year}</p>
+        <p><strong>Director:</strong>${data.Director}</p>
+        <p><strong>Actors</strong>${data.Actors}</p> 
+        </div>
+        `;
+        
+       resultContainer.innerHTML = movieInfo; // change innerHTML text to the api response text
+
+      } else {
+        resultContainer.innerHTML = `<p>An Error Occured`; // show error message if unable to fetch data from api
+
+      }
+    })
+    .catch(error =>{
+      console.log(error);
+      resultContainer.innerHTML = `<p>An Error Occured While Fetching` //show error message in the output div
+    })
+  }
+})
