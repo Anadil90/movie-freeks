284e985feaec6e570c59b0ff276ce6911d69836a
Anadil90 <mashuk.anadil@yahoo.com>
2026-08-19 11:25:42 +0200

    assign fetched data from api to variable searchResponse in order to reuse data for the comments section.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,27 @@
 /*fetch response from api and asign it to a variable*/
-search = "pacific rim"
-const apiResponse = fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=68dd180a&t=${search}`)
-                        .then(response => response.json())
-                        .then(data => {
-                            
-                        })
+    
+const searchResponse = function () {//search results assigned to variable for reusability
+    let search = "pacific rim"
+
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=68dd180a&t=${search}`)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+        
+    //get the elements to represent the returned movie information
+    let movieTitle = $("#movie-title");
+    let movieYear = $(".movie-year");
+    let movieActors = $(".movie-actors");
+    let movieDirector = $(".movie-director");
+    let moviePlot = $("#movie-plot");
+
+    //inject the data to the corresponding elements for movie info
+    movieTitle.text(data.Title);
+    movieYear.text(data.Year);
+    movieActors.text(data.Actors);
+    movieDirector.text(data.Director);
+    moviePlot.text(data.Plot);                  
+    })
+}
+
+searchResponse()
