3a597f4031efc92ea4dc4f052e9553f4a2b6937a
Anadil90 <mashuk.anadil@yahoo.com>
2026-08-19 07:43:45 +0200

    fetch response from api and assign the result to the variable apiResponse.

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+/*fetch response from api and asign it to a variable*/
+search = "pacific rim"
+const apiResponse = fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=68dd180a&t=${search}`)
+                        .then(response => response.json())
+                        .then(data => console.log(data))
