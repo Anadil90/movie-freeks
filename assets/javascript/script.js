5bf8932fd13e873242012358f28a254502950457
Anadil90 <mashuk.anadil@yahoo.com>
2026-08-19 17:16:13 +0200

    set alt atribute to movie poster image in main-content page.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,13 +15,27 @@ const searchResponse = function () {//search results assigned to variable for re
     let movieDirector = $(".movie-director");
     let moviePlot = $("#movie-plot");
 
-    //inject the data to the corresponding elements for movie info
-    movieTitle.text(data.Title);
-    movieYear.text(data.Year);
-    movieActors.text(data.Actors);
-    movieDirector.text(data.Director);
-    moviePlot.text(data.Plot);                  
+
+    //create and append label, along with the data to the corresponding elements for movie info
+    movieTitle.html(`<label class=movie-info-label>Movie: </label> ${data.Title}`);
+    movieYear.html(`<label class=movie-info-label>Year: </label> ${data.Year}`);
+    movieActors.html(`<label class=movie-info-label>Actors: </label> ${data.Actors}`);
+    movieDirector.html(`<label class=movie-info-label>Director: </label> ${data.Director}`);
+    moviePlot.html(`<label class=movie-info-label>Plot: </label> ${data.Plot}`); 
+    
+    //set img attribute to display movie poster
+    $(".movie-poster").attr("src", data.Poster);//set the poster image for the movie
+    $("movie-poster").attr("alt", `Poster image for the movie ${data.Title}`)//show for which movie the poster is for
     })
+
+    
+    //show movie-info element when user clicks on the search movie button, and the functions runs
+    let display = "show"
+
+    if(display  === "show") {
+       $(".movie-info").css({"visibility":"visible"}) 
+    }
+    
 }
 
 searchResponse()
