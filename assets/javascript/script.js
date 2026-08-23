eb177ebab1f31c9cea57d42245f12c8e9ec0acbe
Anadil90 <mashuk.anadil@yahoo.com>
2026-08-23 16:26:48 +0200

    add TMDB logo to attribute TMDB api usage, test and get proper filtered reponse from api request to return only data matching with the search query string.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,41 @@
 document.getElementById("user-search").addEventListener("submit", function(event) {
     let searchInput = event.target.searchInput.value;//get value from user input
-
-    let apiUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=68dd180a&t=${searchInput}`
+    let searchQuery = searchInput;
     
-    event.preventDefault()//prevent the default behaviour of the form
-    /*fetch response from api and append the append the resulting data to elements*/
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data, searchInput)
+    const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWU0MjUyN2NlYTEwZjZkMmIyZjc5YzdkZDliNWU3MSIsIm5iZiI6MTc4NzI1MDQzMi41MjE5OTk4LCJzdWIiOiI2YTg3NDcwMGEyYmI0NWZjZTVkZmFmNTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ux6TU9ZcnY24Tige53uoG8PRodfROzF9FOWYJVmW3r4'} //... here refers to the read access token given to TMDB user
+        };
+    
+    event.preventDefault()//Prevent the default behaviour of the form
+    /*Fetch response from api and append the append the resulting data to elements*/
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(data =>  {
+        for(result of data.results) {//loop through the api response and assign the data to variable result
 
-                //get the elements to represent the returned movie information
-                let movieTitle = $("#movie-title");
+            let searchResults;
+            //Filter out the api response data to return only the object correponding to the search query
+            const matches = data.results.filter(//Filter the data response from the api directly and return the result that matches with search query 
+                movie => {
+
+                const originalTitle = movie.original_title.toLowerCase();
+                const langIsEnglish = movie.original_language === "en";
+                const searchQueryLowerCased = searchQuery.toLowerCase();
+                
+                if(originalTitle === searchQueryLowerCased && langIsEnglish) {
+                    console.log("found match: ", originalTitle, movie)
+                    return movie.original_title.toLowerCase() === searchQuery.toLowerCase()//Transform to lowercase to perform strict matching
+                }
+                    
+                });
+                
+
+        }
+    })
+    .catch(err => console.error(err));
+             //get the elements to represent the returned movie information
+              /*  let movieTitle = $("#movie-title");
                 let movieYear = $(".movie-year");
                 let movieActors = $(".movie-actors");
                 let movieDirector = $(".movie-director");
@@ -35,6 +59,7 @@ document.getElementById("user-search").addEventListener("submit", function(event
                     $(".movie-comments").css({"visibility": "visible"})//show the movie comments div 
 
         
-        })
+    
        event.target.searchInput.value = ""//reset the input field
+       */
 })
