fe79b41b5b3209150df3618a2ff51fa63bd7b40f
Anadil90 <mashuk.anadil@yahoo.com>
2026-08-25 22:26:23 +0200

    include another fetch method to retrieve the movie cast from TMDB credits endpoint in script.js, display movie actors in the movie-info div.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,20 +6,21 @@ document.getElementById("user-search").addEventListener("submit", function(event
             method: 'GET',
             headers: {accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWU0MjUyN2NlYTEwZjZkMmIyZjc5YzdkZDliNWU3MSIsIm5iZiI6MTc4NzI1MDQzMi41MjE5OTk4LCJzdWIiOiI2YTg3NDcwMGEyYmI0NWZjZTVkZmFmNTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ux6TU9ZcnY24Tige53uoG8PRodfROzF9FOWYJVmW3r4'} //... here refers to the read access token given to TMDB user
         };
-    
+    let movieId;
+    let apiUrl = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`;//retrieves the movie data
+
     event.preventDefault()//Prevent the default behaviour of the form
     /*Fetch response from api and append the append the resulting data to elements*/
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`, options)
+    
+    fetch(apiUrl, options)
     .then(res => res.json())
     .then(data =>  {
         for(result of data.results) {//loop through the api response and assign the data to variable result
             console.log(data.results)
 
-            
             //Filter out the api response data to return only the object correponding to the search query
              data.results.filter(//Filter the data response from the api directly and return the result that matches with search query 
                 movie => {
-                    
                     let originalTitle = movie.original_title.toLowerCase();
                     const langIsEnglish = movie.original_language === "en";
                     const searchQueryLowerCased = searchQuery.toLowerCase();
@@ -37,17 +38,40 @@ document.getElementById("user-search").addEventListener("submit", function(event
                         /*console.log("found match: ", originalTitle, movie)*/
                         movie.original_title.toLowerCase() === searchQueryLowerCased//Transform to lowercase to perform strict matching
                          //get the elements to represent the returned movie information
-                let movieTitle = $("#movie-title");
-                let movieYear = $(".movie-year");
-                let movieActors = $(".movie-actors");
-                let movieDirector = $(".movie-director");
-                let moviePlot = $("#movie-plot");
+                    let movieTitle = $("#movie-title");
+                    let movieYear = $(".movie-year");
+                    let movieActors = $(".movie-actors");
+                    let movieDirector = $(".movie-director");
+                    let moviePlot = $("#movie-plot");
                 
-                console.log(searchInput)
+                    console.log(searchInput)
                     //create and append label, along with the data to the corresponding elements for movie info
                     movieTitle.html(`<label class=movie-info-label>Movie: </label> ${movie.original_title}`);
                     movieYear.html(`<label class=movie-info-label>Year: </label> ${movie.release_date}`);
-                    movieActors.html(`<label class=movie-info-label>Actors: </label>`);
+
+                    /*TMDB api does not allow appending query parameters to the api query string with append_to_response. Therefore, a seperate api call
+                    to the credits endpoint has to be made to retrieve movie credits*/ 
+                    //fetch movie credits from the TMDB credits endpoint
+                    fetch(`https://api.themoviedb.org/3/movie/${movie.id}/credits`, options)
+                    .then(res => res.json())
+                    .then(credits => {
+                        /*loop through and return movie cast. Break off loop at the fifth iteration to show the first 5 actors as main actors.*/
+                        for(i = 0; i < credits.cast.length; i++) {
+                            console.log("actor", credits.cast[i].original_name, )
+                            let actors = credits.cast.slice(0, 4);
+                                console.log(actors)
+                              movieActors.html(`<label class=movie-info-label>Actors: </label>${actors[0].name}, ${actors[1].name}, ${actors[2].name},
+                              ${actors[3].name}
+                             `);
+                            if(i < 4) {
+                                continue
+                            }
+                            else {
+                                break
+                            }
+                        }
+                    })
+                    
                     movieDirector.html(`<label class=movie-info-label>Director: </label>`);
                     moviePlot.html(`<label class=movie-info-label>Plot: </label> ${movie.overview}`); 
                     //set img attribute to display movie poster
@@ -57,21 +81,16 @@ document.getElementById("user-search").addEventListener("submit", function(event
                     //show for which movie the poster is for
                     $("movie-poster").attr("alt", `Poster image for the movie ${movie.original_title}`)
 
-                    $(".movie-info").css({"visibility": "visible"})//show the .movie-info div with the search results
+                    
                     $(".post-comment").css({"visibility": "visible"})//show post comment button with search results
                     $(".movie-comments").css({"visibility": "visible"})//show the movie comments div
                     }
-                
-                   
-                
-            });
-                /*let matchedMovie = Object.entries(results);
-                let searchResults;
-                    console.log("matched", matchedMovie.slice(0, 1))*/
-            //Loop through the array of duplicate responses from the search query and return only the first result
-             
 
-            
+                    else {
+                        $("movie-info").text("The movie you searched for was not found. Please make sure that the movie title has been typed properly.")
+                        $(".movie-info").css({"visibility": "visible"})//show the .movie-info div with the search results
+                    }
+            });
         }
     })
     .catch(err => console.error(err));
