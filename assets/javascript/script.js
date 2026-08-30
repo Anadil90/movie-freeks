3003cacb5cd1a4e62350b169a87e11b9e081ae44
Anadil90 <mashuk.anadil@yahoo.com>
2026-08-30 02:29:35 +0200

    remove the display:hidden property from the movie-info, movie-comments, and post-comment style declarations in style.css, hide all three aforementioned elements with jquery .hide() method in script.js and then show them again with by calling .show() at the end of the submit event listener in script.js.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,16 @@
+$(".movie-info").hide();//Hide the movi-info element from view
+$(".post-comment").hide()//Hide the display of the movie comments button
+$(".movie-comments").hide()//Hide the display of the movie comments section by default
+
 document.getElementById("user-search").addEventListener("submit", function(event) {
     let searchInput = event.target.searchInput.value;//get value from user input
     let searchQuery = searchInput;
-    
+
     const options = {
-            method: 'GET',
-            headers: {accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWU0MjUyN2NlYTEwZjZkMmIyZjc5YzdkZDliNWU3MSIsIm5iZiI6MTc4NzI1MDQzMi41MjE5OTk4LCJzdWIiOiI2YTg3NDcwMGEyYmI0NWZjZTVkZmFmNTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ux6TU9ZcnY24Tige53uoG8PRodfROzF9FOWYJVmW3r4'} //... here refers to the read access token given to TMDB user
-        };
+        method: 'GET',
+        headers: {accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWU0MjUyN2NlYTEwZjZkMmIyZjc5YzdkZDliNWU3MSIsIm5iZiI6MTc4NzI1MDQzMi41MjE5OTk4LCJzdWIiOiI2YTg3NDcwMGEyYmI0NWZjZTVkZmFmNTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ux6TU9ZcnY24Tige53uoG8PRodfROzF9FOWYJVmW3r4'} //... here refers to the read access token given to TMDB user
+    };
+
     let movieId;
     let apiUrl = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`;//retrieves the movie data
 
@@ -38,71 +43,70 @@ document.getElementById("user-search").addEventListener("submit", function(event
                         /*console.log("found match: ", originalTitle, movie)*/
                         movie.original_title.toLowerCase() === searchQueryLowerCased//Transform to lowercase to perform strict matching
                          //Get the elements to represent the returned movie information
-                    let movieTitle = $("#movie-title");
-                    let movieYear = $(".movie-year");
-                    let movieActors = $(".movie-actors");
-                    let movieDirector = $(".movie-director");
-                    let moviePlot = $("#movie-plot");
-                
-                    console.log(searchInput)
-                    //Create and append label, along with the data to the corresponding elements for movie info
-                    movieTitle.html(`<label class=movie-info-label>Movie: </label> ${movie.original_title}`);//Movie title
-                    movieYear.html(`<label class=movie-info-label>Year: </label> ${movie.release_date}`);//Movie release year
+                        let movieTitle = $("#movie-title");
+                        let movieYear = $(".movie-year");
+                        let movieActors = $(".movie-actors");
+                        let movieDirector = $(".movie-director");
+                        let moviePlot = $("#movie-plot");
+                    
+                        console.log(searchInput)
+                        //Create and append label, along with the data to the corresponding elements for movie info
+                        movieTitle.html(`<label class=movie-info-label>Movie: </label> ${movie.original_title}`);//Movie title
+                        movieYear.html(`<label class=movie-info-label>Year: </label> ${movie.release_date}`);//Movie release year
 
-                    /*TMDB api does not allow appending query parameters to the api query string with append_to_response. Therefore, a seperate api call
-                    to the credits endpoint has to be made to retrieve movie credits*/ 
-                    //Fetch movie credits from the TMDB credits endpoint
-                    fetch(`https://api.themoviedb.org/3/movie/${movie.id}/credits`, options)
-                    .then(res => res.json())
-                    .then(credits => {
-                        /*Loop through and return movie cast. Break off loop at the fifth iteration to show the first 4 actors as main actors.*/
-                        for(i = 0; i < credits.cast.length; i++) {
-                            console.log("actor", credits.cast[i].original_name, )
-                            let actors = credits.cast.slice(0, 4);
-                                console.log(actors)
+                        /*TMDB api does not allow appending query parameters to the api query string with append_to_response. Therefore, a seperate api call
+                        to the credits endpoint has to be made to retrieve movie credits*/ 
+                        //Fetch movie credits from the TMDB credits endpoint
+                        fetch(`https://api.themoviedb.org/3/movie/${movie.id}/credits`, options)
+                        .then(res => res.json())
+                        .then(credits => {
+                            /*Loop through and return movie cast. Break off loop at the fifth iteration to show the first 4 actors as main actors.*/
+                            for(i = 0; i < credits.cast.length; i++) {
+                                console.log("actor", credits.cast[i].original_name, )
+                                let actors = credits.cast.slice(0, 4);
+                                    console.log(actors)
 
-                                //Show movie actors on the actors label element
-                                movieActors.html(`<label class=movie-info-label>Actors: </label> ${actors[0].name}, ${actors[1].name}, ${actors[2].name},
-                                ${actors[3].name}`);
+                                    //Show movie actors on the actors label element
+                                    movieActors.html(`<label class=movie-info-label>Actors: </label> ${actors[0].name}, ${actors[1].name}, ${actors[2].name},
+                                    ${actors[3].name}`);
 
-                                //Show movie director on the director label element
-                                movieDirector.html(`<label class=movie-info-label>Director: </label> ${credits.crew[1].name}`);
-                                console.log(credits)
-                            if(i < 4) {
-                                continue
+                                    //Show movie director on the director label element
+                                    movieDirector.html(`<label class=movie-info-label>Director: </label> ${credits.crew[1].name}`);
+                                    console.log(credits)
+                                if(i < 4) {
+                                    continue
+                                }
+                                else {
+                                    break
+                                }
                             }
-                            else {
-                                break
-                            }
-                        }
+                            
+                        })
                         
-                    })
-                    
-                    
-                    moviePlot.html(`<label class=movie-info-label>Plot: </label> ${movie.overview}`); 
-                    //set img attribute to display movie poster
-                    const posterUrlBase = "https://image.tmdb.org/t/p/w500/";
-                    $(".movie-poster").attr("src", posterUrlBase + movie.poster_path);//set the poster image for the movie
+                        
+                        moviePlot.html(`<label class=movie-info-label>Plot: </label> ${movie.overview}`); 
+                        //set img attribute to display movie poster
+                        const posterUrlBase = "https://image.tmdb.org/t/p/w500/";
+                        $(".movie-poster").attr("src", posterUrlBase + movie.poster_path);//set the poster image for the movie
 
-                    //show for which movie the poster is for
-                    $("movie-poster").attr("alt", `Poster image for the movie ${movie.original_title}`)
+                        //show for which movie the poster is for
+                        $("movie-poster").attr("alt", `Poster image for the movie ${movie.original_title}`)
 
-                    
-                    $(".post-comment").css({"visibility": "visible"})//show post comment button with search results
-                    $(".movie-comments").css({"visibility": "visible"})//show the movie comments div
+                        
+                        $(".post-comment").show()//show post comment button with search results
+                        $(".movie-comments").show()//show the movie comments div
                     }
 
                     else {
                         $("movie-info").text("The movie you searched for was not found. Please make sure that the movie title has been typed properly.")
-                        $(".movie-info").css({"visibility": "visible"})//show the .movie-info div with the search results
+                        $(".movie-info").show()//show the .movie-info div with the search results
                     }
             });
         }
     })
     .catch(err => console.error(err));
-             
-        
-    
-       event.target.searchInput.value = ""//reset the input field
+
+    //reset the input field           
+    event.target.searchInput.value = ""
        
 })
